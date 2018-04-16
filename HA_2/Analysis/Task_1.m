44895b2cbcc0de615420b0de390c409dc5db22be
GitKiller123 <jonasalexandersson@hotmail.com>
2018-04-16 21:46:43 +0200

    Rapport

--- HA_2/Analysis/Task_1.m
+++ HA_2/Analysis/Task_1.m
@@ -40,23 +40,30 @@ switch Task
         z = [min([Xhat(4) Xhat(9) Xhat(15)]) - 6:0.01:max([Xhat(4) Xhat(9) Xhat(15)]) + 6];
         XhatPdf_1 = normpdf(z, Xhat(:,4), P(:,:,4));
         XhatPdf_2 = normpdf(z, Xhat(:,9), P(:,:,9));
-        XhatPdf_3 = normpdf(z, Xhat(:,15), P(:,:,15));
+        XhatPdf_3 = normpdf(z, Xhat(:,14), P(:,:,15));
         figure(2)
         subplot(3,1,1)
         plot([X(:,5) X(:,5)], [0 0.5],'b')
         hold all
         plot(z, XhatPdf_1, 'k')
-        legend('True state', 'Posterior density')
+        plot([Y(:,4) Y(:,4)], [0 0.5],'r--')
+        legend('True state', 'Posterior density', 'Measurement')
         subplot(3,1,2)
         plot([X(:,10) X(:,10)], [0 0.5],'b')
         hold all
         plot(z, XhatPdf_2, 'k')
-        legend('True state', 'Posterior density')
+        plot([Y(:,9) Y(:,9)], [0 0.5],'r--')
+        legend('True state', 'Posterior density', 'Measurement')
         subplot(3,1,3)
         plot([X(:,15) X(:,15)], [0 0.5],'b')
         hold all
         plot(z, XhatPdf_3, 'k')
-        legend('True state', 'Posterior density')
+        plot([Y(:,14) Y(:,14)], [0 0.5],'r--')
+        legend('True state', 'Posterior density', 'Measurement')
+        figure(3)
+        hist(X-[x_0 Xhat])
+        hold all
+        plot(0:N, [3*sqrt(cov(X,[x_0 Xhat])) 3*sqrt(cov(X,[x_0 Xhat]))])
     case {'c'}
         z = [min([Xhat(k-1) Xhat(k)])-8:0.01:max([Xhat(k-1) Xhat(k)])+8];
         xpdf_1 = normpdf(z,Xhat(k-1),P(:,:,k-1));
@@ -65,10 +72,11 @@ switch Task
         xpdf_3 = normpdf(z, Xhat(k), P(:,:,k));
         plot(z, xpdf_1)
         hold all
-        plot(z, xpdf_2,'r--')
-        plot(z, xpdf_3,'r')
-        plot([Y(k) Y(k)], [0 max([xpdf_1 xpdf_2 xpdf_3])],'k')
-        legend('k - 1 Updated', 'k Predicted', 'k Updated', 'y_k')
+        plot(z, xpdf_2,'k--')
+        plot(z, xpdf_3,'k')
+        plot([Y(k) Y(k)], [0 max([xpdf_1 xpdf_2 xpdf_3])],'r')
+        legend('p(x_{k-1}|y_{1:k-1})', 'p(x_k|y_{1:k-1})', 'p(x_k|y_{1:k})', 'y_k')
+        %legend('k - 1 Updated', 'k Predicted', 'k Updated', 'y_k')
     case {'d'}
         Xerror = (X(:,2:end) - Xhat);
         z = [-5:0.01:5];
