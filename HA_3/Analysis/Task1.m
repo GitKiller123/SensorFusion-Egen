d534ba2905d433d84b63f64b8f9c7d6a536bbd6e
GitKiller123 <jonasalexandersson@hotmail.com>
2018-04-27 20:11:59 +0200

    HA3

--- HA_3/Analysis/Task1.m
+++ HA_3/Analysis/Task1.m
@@ -34,10 +34,14 @@ Y1_corr = ang2pos(Y1, s1, s2);
 Y2_corr = ang2pos(Y2, s1, s2);
 
 %Task 1a
-E_Y1 = mean(Y1_corr');
-E_Y2 = mean(Y2_corr');
-Y1_COV = cov(Y1_corr');
-Y2_COV = cov(Y2_corr');
+E_Y1 = mean(Y1')';
+E_Y2 = mean(Y2')';
+Y1_COV = cov(Y1');
+Y2_COV = cov(Y2');
+E_Y1_corr = mean(Y1_corr')';
+E_Y2_corr = mean(Y2_corr')';
+Y1_COV_corr = cov(Y1_corr');
+Y2_COV_corr = cov(Y2_corr');
 
 Run_plot = true;
 
@@ -47,13 +51,40 @@ for i = 1:3
     [task1.(type{i}).x2, task1.(type{i}).P2] = nonLinKFprediction(x_02, P_02,f, Q, type{i});
     task1.(type{i}).x1corr = ang2pos(task1.(type{i}).x1, s1, s2);
     task1.(type{i}).x2corr = ang2pos(task1.(type{i}).x2, s1, s2);
+    if Run_plot
+        xy_x1 = sigmaEllipse2D(task1.(type{i}).x1, task1.(type{i}).P1, 3);
+        xy_y1 = sigmaEllipse2D(E_Y1, Y1_COV, 3);
+        xy_x2 = sigmaEllipse2D(task1.(type{i}).x2, task1.(type{i}).P2, 3);
+        xy_y2 = sigmaEllipse2D(E_Y2, Y2_COV, 3);
+        figure('units','normalized','outerposition',[0 0 1 1])
+        subplot(1,2,1)
+        plot(Y1(1,:),Y1(2,:),'ro')
+        hold all
+        plot(xy_y1(1,:),xy_y1(2,:),'r--')
+        plot(task1.(type{i}).x1(1), task1.(type{i}).x1(2),'kx')
+        plot(xy_x1(1,:),xy_x1(2,:),'k--')
+        header = 'Samples and densities for ' + string(type{i}) + ' case 1';
+        title(header)
+        legend('Measurements','3-Sigma Measurement', 'Approx mean', '3-Sigma Approx')
+        
+        subplot(1,2,2)
+                plot(Y2(1,:),Y2(2,:),'ro')
+        hold all
+        plot(xy_y2(1,:),xy_y2(2,:),'r--')
+        plot(task1.(type{i}).x2(1), task1.(type{i}).x2(2),'kx')
+        plot(xy_x2(1,:),xy_x2(2,:),'k--')
+        header = 'Samples and densities for ' + string(type{i}) + ' case 2';
+        title(header)
+        legend('Measurements','3-Sigma Measurement', 'Approx mean', '3-Sigma Approx')
+        
+        hgexport(gcf, ['Task1_fig_B_' type{i} '.jpg'], hgexport('factorystyle'), 'Format', 'fig');
+    end
 end
+
 
 %Task 1c-e
 %Plot shit, analyze etc.
-if Run_plot
-    
-end
+
 
 
 
--- HA_3/Analysis/Task2_ny.m
+++ HA_3/Analysis/Task2_ny.m
@@ -16,7 +16,8 @@ Sigma_s1 = [(10*pi/180) (0.5*pi/180)];
 Sigma_s2 = (0.5*pi/180);
 Q = diag([0 0 Sigma_v^2 0 Sigma_w^2]);
 
-run_plot = true;
+run_plot = false;
+error_try = true;
 N = 100;
 Case = {'1' '2'};
 
@@ -29,9 +30,13 @@ for l = 1:2
     for i = 1:3
         [task2.([type{i} '_' Case{l}]).xf,task2.([type{i} '_' Case{l}]).Pf,task2.([type{i} '_' Case{l}]).xp,task2.([type{i} '_' Case{l}]).Pp] = nonLinearKalmanFilter(task2.(['Y_' Case{l}]), x_0, P_0 , f, Q, h, R, type{i});
         %task2.([type{i} '_' Case{l}]).xfpos = ang2pos(task2.([type{i} '_' Case{l}]).xf, s1, s2);
-        
-        if run_plot
-            figure(i+(l-1)*3)
+    end
+end
+
+if run_plot
+    for i = 1:3
+        for l = 1:2
+            figure(i)
             plot(task2.(['X_' Case{l}])(1,:), task2.(['X_' Case{l}])(2,:))
             header = ['Case ' Case{l} ' ' type{i}];
             title(header)
@@ -42,27 +47,40 @@ for l = 1:2
                 xy = sigmaEllipse2D( task2.([type{i} '_' Case{l}]).xf(1:2,k*5), task2.([type{i} '_' Case{l}]).Pf(1:2,1:2,k*5), 3);
                 plot(xy(1,:),xy(2,:),'--','color',[0 0.5 0])
             end
-
             legend('True states', 'Measurements', 'Estimates','3-Sigma level')
         end
     end
 end
 
-for i = 1:3
-    task2.([type{i} '_Error']).Error = [];
-end
 
-R = [Sigma_s1(1)^2 0;
-        0 Sigma_s2^2];
-for k = 1:100
-    task2.X_Error = genNonLinearStateSequence(x_0, P_0, f, Q, N);
-    task2.Y_Error = genNonLinearMeasurementSequence(task2.X_Error, h, R);
-    task2.Ypos_Error = ang2pos(task2.Y_Error, s1, s2);
-    for i =1:3
-        [task2.([type{i} '_Error']).xf,task2.([type{i} '_Error']).Pf,task2.([type{i} '_Error']).xp,task2.([type{i} '_Error']).Pp] = nonLinearKalmanFilter(task2.(['Y_Error']), x_0, P_0 , f, Q, h, R, type{i});
-        task2.([type{i} '_Error']).xfpos = ang2pos(task2.([type{i} '_Error']).xf, s1, s2);
-        task2.([type{i} '_Error']).Error = [task2.([type{i} '_Error']).Error; task2.([type{i} '_Error']).xfpos - task2.X_Error(1:2,2:end)];
+if error_try
+    for l = 1:2
+        for i = 1:3
+            task2.([type{i} '_Error_' Case{l}]).Error = [];
+        end
+    end
+    for k = 1:100
+        task2.X_Error = genNonLinearStateSequence(x_0, P_0, f, Q, N);
+        for l = 1:2
+            R = [Sigma_s1(l)^2 0;
+                    0 Sigma_s2^2];
+            task2.Y_Error = genNonLinearMeasurementSequence(task2.X_Error, h, R);
+            for i =1:3
+                try
+                    [task2.([type{i} '_Error_' Case{l}]).xf,task2.([type{i} '_Error']).Pf,task2.([type{i} '_Error']).xp,task2.([type{i} '_Error']).Pp] = nonLinearKalmanFilter(task2.Y_Error, x_0, P_0 , f, Q, h, R, type{i});
+                catch
+%                     task2.X_Error = genNonLinearStateSequence(x_0, P_0, f, Q, N);
+                    task2.Y_Error = genNonLinearMeasurementSequence(task2.X_Error, h, R);
+                    [task2.([type{i} '_Error_' Case{l}]).xf,task2.([type{i} '_Error']).Pf,task2.([type{i} '_Error']).xp,task2.([type{i} '_Error']).Pp] = nonLinearKalmanFilter(task2.Y_Error, x_0, P_0 , f, Q, h, R, type{i});
+                end
+                task2.([type{i} '_Error_' Case{l}]).Error = [task2.([type{i} '_Error_' Case{l}]).Error; task2.([type{i} '_Error_' Case{l}]).xf(1:2,:) - task2.X_Error(1:2,2:end)];
+            end
+        end
+    end
+    for l = 1:2
+        task2.([type{1} '_Error_' Case{l}]).Error = mean(task2.([type{1} '_Error_' Case{l}]).Error);
+        task2.([type{2} '_Error_' Case{l}]).Error = mean(task2.([type{2} '_Error_' Case{l}]).Error);
+        task2.([type{3} '_Error_' Case{l}]).Error = mean(task2.([type{3} '_Error_' Case{l}]).Error);
     end
 end
-     
 
